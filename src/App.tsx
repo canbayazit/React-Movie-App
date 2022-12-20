f22d5379a930f1caf2150a11f0b2c79978674e5b
Can <rovahelsing@gmail.com>
2022-12-20 04:37:28 +0300

    Folder Structure Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import { Routes } from "react-router";
+import { Route } from "react-router/dist/lib/components";
+import Home from "./Pages/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/movies" element={<Home />}>
+          <Route path="movies/categories/:id" element={<Home />}></Route>
+        </Route>
+        <Route path="/movies/:id" element={<Home />}></Route>
+        <Route path="/login" element={<Home />}></Route>
+        <Route path="/register" element={<Home />}></Route>
+        <Route path="/favourites" element={<Home />}></Route>
+        <Route path="/person" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Pages/DetailPage/MovieDetail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './home.module.scss';
+const MovieDetail = () => {
+  return (
+    <div>MovieDetail</div>
+  )
+}
+
+export default MovieDetail
