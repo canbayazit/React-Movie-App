5b950204a0b27b0bce384ad38264ecc75fd19226
Can <rovahelsing@gmail.com>
2023-02-06 02:49:23 +0300

    if user has not profil image, put user image png + css

--- src/Components/NavContents/Cast/Cast.tsx
+++ src/Components/NavContents/Cast/Cast.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import { imageSize } from '../../../Store/constant';
-import { useGetCreditServiceQuery } from '../../../Store/services';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { imageSize } from "../../../Store/constant";
+import { useGetCreditServiceQuery } from "../../../Store/services";
+import user from '../../../Assets/img/user.png'
 import styles from "./cast.module.scss";
 
 const Cast = () => {
@@ -10,22 +11,22 @@ const Cast = () => {
 
   return (
     <div className={styles.container_nav_content_cast}>
-          {credit.data?.cast.map((item, i) => (
-            <div
-              key={`${i}_${item.name}`}
-              className={styles.container_nav_content_cast_profil}
-            >
-              <div className={styles.container_nav_content_cast_profil_img}>
-                <img src={`${imageSize}${item.profile_path}`} alt="" />
-              </div>
-              <div className={styles.container_nav_content_cast_profil_info}>
-                <h4>{item.original_name}</h4>
-                <span>{item.character}</span>
-              </div>
-            </div>
-          ))}
+      {credit.data?.cast.map((item, i) => (
+        <div
+          key={`${i}_${item.name}`}
+          className={styles.container_nav_content_cast_profil}
+        >
+          <div className={styles.container_nav_content_cast_profil_img}>
+            <img src={item.profile_path ? `${imageSize}${item.profile_path}`:user} alt="" />
+          </div>
+          <div className={styles.container_nav_content_cast_profil_info}>
+            <h4>{item.original_name}</h4>
+            <span>{item.character}</span>
+          </div>
         </div>
-  )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Cast
+export default Cast;
