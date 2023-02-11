5f6844ebe12f3b70a1df23614a678e4850cb3a76
Can <rovahelsing@gmail.com>
2023-02-11 17:31:57 +0300

    updated navList added person navlist to navList component

---
+++ src/Components/List/NavList.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import Cast from "./NavContents/Cast/Cast";
+import Comment from "./NavContents/Comments/Comment";
+import Crew from "./NavContents/Crew/Crew";
+import Suggested from "./NavContents/Suggested/Suggested";
+import styles from "./list.module.scss";
+import { useParams } from "react-router";
+import Known from "./NavContents/Known/Known";
+import Detail from "./NavContents/Detail/Detail";
+
+interface INavItem {
+  id: number;
+  name: string;
+  component: JSX.Element;
+}
+
+interface INav {
+  movie: INavItem[];
+  person:INavItem[];
+}
+const navList: INav = {
+  movie: [
+    { id: 1, name: "SUGGESTED", component: <Suggested/> },
+    { id: 2, name: "CAST", component: <Cast/> },
+    { id: 3, name: "CREW", component: <Crew/> },
+    { id: 4, name: "COMMENT", component: <Comment/> },
+  ],
+  person: [
+    { id: 1, name: "MOVIES", component: <Known keys="movie" credit="movie_credits"/> },
+    { id: 2, name: "TV SHOWS", component: <Known keys="tv" credit="tv_credits"/> },
+    { id: 3, name: "DETAIL", component: <Detail/> },
+  ],
+};
+const NavList = () => {
+  const {category}=useParams();
+  const [index, setIndex] = useState<number>(0);
+  const handleClick = (i: number) => {
+    setIndex(i);
+  };
+  return (
+    <div className={styles.container}>
+      <nav className={styles.container_nav}>
+        {navList[category as keyof INav].map((item, i) => (
+          <div
+            key={item.id}
+            onClick={() => handleClick(i)}
+            className={
+              index === i
+                ? `${styles.container_nav_bar} ${styles.active}`
+                : styles.container_nav_bar
+            }
+          >
+            {item.name}
+          </div>
+        ))}
+        <div className={styles.indicator}></div>
+      </nav>
+      {navList[category as keyof INav].map((item, i) => (
+        <div
+          key={item.id}
+          className={
+            index === i
+              ? `${styles.container_nav_content} ${styles.active}`
+              : styles.container_nav_content
+          }
+        >
+          {item.component}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NavList;
