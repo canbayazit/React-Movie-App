d67b02540039c709294282460aa9142c81d31fb9
Can <rovahelsing@gmail.com>
2023-02-11 17:01:19 +0300

    added person detail component

---
+++ src/Components/Detail/PersonDetail/PersonDetail.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useParams } from "react-router";
+import { imageSize } from "../../../Store/constant";
+import { useGetDetailServiceQuery } from "../../../Store/services";
+import styles from "./detail.module.scss";
+
+const Detail = () => {
+  const { category, id } = useParams();
+  const { data, isLoading } = useGetDetailServiceQuery({
+    category: category!,
+    id: id!,
+  });
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading</div>
+      ) : (
+        <div className={styles.container}>
+          <div className={styles.container_img}>
+            <img src={`${imageSize}${data?.profile_path}`} alt="" />
+          </div>
+          <section className={styles.container_detail}>
+            <h1>{data?.name}</h1>
+            <div className={styles.container_detail_bio}>
+              <h3>Biography</h3>
+              <p>{data?.biography}</p>
+            </div>
+          </section>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Detail;
