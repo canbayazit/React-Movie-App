1543a7886d311397ba592fefe0db28dd904e036e
Can <rovahelsing@gmail.com>
2023-02-11 16:54:15 +0300

    folder structure changed

---
+++ src/Components/List/NavContents/Comments/Comment.tsx
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import * as Yup from "yup";
+import styles from "./comment.module.scss";
+import user from '../../../../Assets/img/user.png'
+import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from "formik";
+import moment from "moment";
+import { plane } from "../../../../Assets/svg/icons/paperPlane";
+interface IValues {
+  name: string;
+  email: string;
+  description: string;
+  date: string;
+}
+const commentSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "The password is too short")
+    .required("Name is required"),
+  email: Yup.string()
+    .email("The email is invalid")
+    .required("Mail is required"),
+  description: Yup.string()
+    .min(6, "The description is too short")
+    .required("Description is required")
+    .trim(),
+});
+
+const Comment = () => {
+  const [comment, setComment] = useState<IValues[]>([]);
+  const initialValues: IValues = {
+    name: "",
+    email: "",
+    description: "",
+    date: "",
+  };
+  return (
+    <div className={styles.container}> 
+      <Formik
+        initialValues={initialValues}
+        validationSchema={commentSchema}
+        validateOnChange={false}
+        validateOnBlur={false}
+        onSubmit={(
+          values: IValues
+          // actions,
+          // validate : FormikHelpers<IValues>
+          // { setSubmitting }: FormikHelpers<IValues>
+        ) => {
+          // setTimeout(() => {
+          //   alert(JSON.stringify(values, null, 2));
+          //   setSubmitting(false);
+          // }, 500);
+          console.log({ values });
+          let date = moment.utc().format();
+          setComment([
+            ...comment,
+            {
+              name: values.name,
+              email: values.email,
+              description: values.description,
+              date: date,
+            },
+          ]);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className={styles.container_form}>
+            <div className={styles.container_form_text}>
+              <Field
+                className={styles.container_form_text_field}
+                id="name"
+                name="name"
+                placeholder="Name"
+              />
+              <Field
+                className={styles.container_form_text_field}
+                id="email"
+                name="email"
+                placeholder="name@mail.com"
+                type="email"
+              />
+            </div>
+            <div className={styles.container_form_description}>
+              <Field
+                className={styles.container_form_description_field}
+                id="description"
+                name="description"
+                as="textarea"
+                placeholder="Add a comment..."
+              />
+            </div>
+            <div className={styles.container_form_action}>
+              <div className={styles.container_form_action_error}>
+                {errors.name && touched.name ? (
+                  <div className={styles.container_form_action_error_message}>
+                    {errors.name}
+                  </div>
+                ) : null}
+
+                {errors.email && touched.email ? (
+                  <div className={styles.container_form_action_error_message}>
+                    {errors.email}
+                  </div>
+                ) : null}
+
+                {errors.description && touched.description ? (
+                  <div className={styles.container_form_action_error_message}>
+                    {errors.description}
+                  </div>
+                ) : null}
+              </div>
+              <div className={styles.container_form_action_button}>
+                <button type="submit">
+                  <span>{plane()}</span> Submit
+                </button>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      {comment.length > 0 ? (
+        <div className={styles.container_user}>
+          <div className={styles.container_user_header}>
+            <i className="fa fa-start"></i>
+            <h2>All Comments</h2>
+          </div>
+          <ul>
+            {comment.map((item, i) => (
+              <li>
+                <div className={styles.container_user_img}>
+                  <img src={user} alt="" />
+                </div>
+                <div className={styles.container_user_comment}>
+                  <div className={styles.container_user_comment_info}>
+                    <span>{item.name}</span>
+                    <time dateTime={item.date}>
+                      {moment.utc(item.date).local().startOf('seconds').fromNow()}
+                    </time>
+                  </div>
+                  <p>{item.description}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Comment;
---
+++ src/Components/List/NavContents/Crew/Crew.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { imageSize } from "../../../../Store/constant";
+import { useGetCreditServiceQuery } from "../../../../Store/services";
+import { ICrew } from "../../../../Types/credit";
+import styles from "./crew.module.scss";
+
+const Crew = () => {
+  const [group, setGroup] = useState<string[]>([]);
+  const { category, id } = useParams();
+  const credit = useGetCreditServiceQuery({ category: category!, id: id! });
+
+  const groupPerson = <K extends string | number | symbol, V>(
+    array: V[],
+    group: (person: V) => K
+  ) => {
+    let groupPerson = array.reduce((store, person) => {
+      let key = group(person);
+      if (!store[key]) {
+        store[key] = [];
+      }
+      store[key].push(person);
+      return store;
+    }, {} as Record<K, V[]>);
+
+    let dataArray = [];
+    for (const key in groupPerson) {
+      if (Object.prototype.hasOwnProperty.call(groupPerson, key)) {
+        const value = groupPerson[key];
+        dataArray.push({ group: key, value: value });
+      }
+    }
+    return dataArray;
+  };
+  const getMoreItems = (data: ICrew[], groupItem: string) => {
+    if (group) {
+      //data departmanlardan oluşan personlar
+      console.log(group.includes(groupItem),"groupItem")
+      return group.includes(groupItem)
+        ? data
+        : data.slice(0, 12);
+    }
+    return data.slice(0, 12);
+  };
+  const handleClick = (groupItem: string) => {
+    console.log(group, "group");
+    if (group.includes(groupItem)) {
+    const filterArray=group.filter((item): item is string=> item!==groupItem);
+      setGroup(filterArray)
+    } else {
+      setGroup(prevNames =>  [...prevNames, groupItem]);
+    }
+    
+  };
+  return (
+    <div className={styles.container}>
+      {credit.isLoading ? (
+        "loading"
+      ) : (
+        <div className={styles.container_crew}>
+          <div className={styles.container_crew_director}>
+            {credit.data?.crew
+              .filter((item) => item.job === "Director")
+              .map((item, i) => (
+                <>
+                  <div className={styles.container_crew_director_img}>
+                    <h1>Director</h1>
+                    <img src={`${imageSize}${item.profile_path}`} alt="" />
+                  </div>
+                  <div className={styles.container_crew_director_info}>
+                    <h1>{item.name}</h1>
+                    <span>{item.department}</span>
+                  </div>
+                </>
+              ))}
+          </div>
+          <div className={styles.container_crew_others}>
+            {groupPerson(credit.data?.crew!, (group) => group.department).map(
+              (item, i) => (
+                <div
+                  key={`${item.group}_${i}`}
+                  className={styles.container_crew_others_group}
+                >
+                  <h3>{item.group}</h3>
+                  <ul>
+                    {getMoreItems(item.value, item.group).map((person, i) => (
+                      <li key={`${person.id}_${i}`}>{person.name}</li>
+                    ))}
+                  </ul>
+                  {item.value.length > 12 ? (
+                    <button onClick={() => handleClick(item.group)}>
+                      {group.includes(item.group) ? "...See less": "...See more"}
+                    </button>
+                  ) : null}
+                </div>
+              )
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Crew;
