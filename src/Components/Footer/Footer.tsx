20c49cde2e1eb3f9de687d7fd1a376cd0c07d441
Can <rovahelsing@gmail.com>
2023-02-14 12:38:49 +0300

    added footer component

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
-// import styles from './footer.module.scss';
+import React from "react";
+import { github } from "../../Assets/svg/icons/github";
+import { linkedin } from "../../Assets/svg/icons/linkedin";
+import styles from "./footer.module.scss";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.container}>
+      <h2>CONTACT ME</h2>
+      <ul className={styles.buttons}>
+        <li>
+          <a
+            href="https://github.com/canbayazit"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {github}
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/canbayazit/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {linkedin}
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
