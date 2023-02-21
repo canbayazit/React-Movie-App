8cb3ba92fecba6516f56fb5b6e8261804e2957ff
Can <rovahelsing@gmail.com>
2023-02-21 14:24:19 +0300

    added favorites and whistlist component route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router";
 import { useEffect } from "react";
 import DetailPage from "./Pages/DetailPage/DetailPage";
 import FilterPage from "./Pages/FilterPage/FilterPage";
+import WhistlistFavoritesPage from "./Components/WhistList/WhistlistFavorites";
 
 function App() {
   useEffect(() => {
@@ -20,7 +21,8 @@ function App() {
         <Route path="/:category/:id" element={<DetailPage />}></Route>
         <Route path="/:category" element={<FilterPage />}></Route>
         <Route path="/login" element={<Home />}></Route>
-        <Route path="/favourites" element={<Home />}></Route>
+        <Route path="/whistlist" element={<WhistlistFavoritesPage />}></Route>
+        <Route path="/favourites" element={<WhistlistFavoritesPage />}></Route>
       </Routes>
       <Footer />
     </>
