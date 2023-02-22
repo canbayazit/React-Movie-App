ae3b4e341dde3390aa0d28eb0318b78477d07c52
Can <rovahelsing@gmail.com>
2023-02-22 13:32:28 +0300

    added search bar and its css on header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,52 +1,93 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { logo } from "../../Assets/svg/svg";
+import { logo } from "../../Assets/svg/icons/logo";
 import styles from "./header.module.scss";
+import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from "formik";
+import { search } from "../../Assets/svg/icons/search";
 interface INavItem {
   id: number;
   name: string;
   to: string;
 }
 const headerList: INavItem[] = [
-  { id: 1, name: "Ana Sayfa", to: "/" },
-  { id: 2, name: "Listelerim", to: "/myList" },
-  { id: 3, name: "Favorilerim", to: "/favorites" },
+  { id: 1, name: "HOME", to: "/" },
+  { id: 2, name: "WATCHLIST", to: "/whistlist" },
+  { id: 3, name: "FAVORITES", to: "/favorites" },
 ];
+interface IValues {
+  query: string;
+}
 const Header = () => {
   const [isScrolling, setScrolling] = useState(false);
+  const [query, setQuery] = useState<string>("");
   useEffect(() => {
     window.addEventListener("scroll", () => {
       window.scrollY >= 80 ? setScrolling(true) : setScrolling(false);
     });
-  }, [])
-  
+  }, []);
+  const initialValues: IValues = {
+    query: "",
+  };
   return (
-    <header className={isScrolling ? `${styles.container} ${styles.active}` :styles.container}>
-      <div className={styles.container_logo}>
-        <NavLink to={"/"}>
-          <span>{logo()}</span>
-          <h1>
-            Film<span>Makinası</span>
-          </h1>
-        </NavLink>
-      </div>
-      <div className={styles.container_login}>
-        {headerList.map((item: INavItem) => (
-          <NavLink
-            key={item.id}
-            to={item.to}
-            // style={({ isActive }) => ({
-            //   // borderBottom: isActive ? "2px solid #6ecfff" : "#fff",
-            //   paddingBottom: isActive ? "8px" : "",
-            //   textShadow: isActive ? "0 0 30px #6ecfff" : "",
-            // })}
-          >
-            {item.name}
+    <header
+      className={
+        isScrolling ? `${styles.container} ${styles.active}` : styles.container
+      }
+    >
+      <div className={styles.container_left}>
+        <div className={styles.container_left_logo}>
+          <NavLink to={"/"}>
+            <span>{logo()}</span>
+            <h1>
+              Film<span>Makinası</span>
+            </h1>
           </NavLink>
-        ))}
-        <div className={styles.indicator}></div>
-
-        <button>Giriş</button>
+        </div>
+        <div className={styles.container_left_nav}>
+          {headerList.map((item: INavItem) => (
+            <NavLink key={item.id} to={item.to}>
+              {item.name}
+            </NavLink>
+          ))}
+          <div className={styles.indicator}></div>
+        </div>
+      </div>
+      <div className={styles.container_right}>
+        <div className={styles.container_right_search}>
+          <Formik
+            initialValues={initialValues}
+            className={styles.container_right_search_formik}
+            onSubmit={(values: IValues) => {
+              console.log({ values });
+              setQuery(values.query);
+            }}
+          >
+            <Form
+            className={styles.container_right_search_formik_form}
+            >
+              <Field
+                name="search"
+                placeholder="Search for a Movie, Tv Shows, Person ..."
+                type="text"
+                className={styles.container_right_search_formik_form_input}
+              />
+              <button
+                className={styles.container_right_search_formik_form_button}
+                type="submit"
+              >
+                {search()}
+              </button>
+            </Form>
+          </Formik>
+        </div>
+        <button className={styles.container_right_login}>Giriş</button>
       </div>
     </header>
   );
