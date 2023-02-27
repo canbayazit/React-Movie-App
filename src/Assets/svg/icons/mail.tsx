c32674e12a566de3b5943b4a734d304e291e045e
Can <rovahelsing@gmail.com>
2023-02-27 17:24:24 +0300

    added register component with scss design + svg icons

---
+++ src/Assets/svg/icons/mail.tsx
@@ -0,0 +1,27 @@
+export const mail = (styles: any) => (
+  <svg
+    className={styles.svg}
+    fill="#000000"
+    width="20"
+    height="20"
+    viewBox="4 4 15 15"
+    data-name="Flat Line"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      className={styles.path}
+      d="M21,6V8l-9,5L3,8V6A1,1,0,0,1,4,5H20A1,1,0,0,1,21,6Z"
+      fill="rgb(44, 169, 188)"
+      stroke-width={2}
+    ></path>
+    <path
+      className={styles.path}
+      d="M20,19H4a1,1,0,0,1-1-1V6A1,1,0,0,1,4,5H20a1,1,0,0,1,1,1V18A1,1,0,0,1,20,19ZM20,5H4A1,1,0,0,0,3,6V8l9,5,9-5V6A1,1,0,0,0,20,5Z"
+      fill="none"
+      stroke="rgb(0, 0, 0)"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    ></path>
+  </svg>
+);
---
+++ src/Components/Login/Register/Register.tsx
@@ -0,0 +1,137 @@
+import React, { useEffect } from "react";
+import * as Yup from "yup";
+import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from "formik";
+import styles from "./register.module.scss";
+import { Link } from "react-router-dom";
+import { lock } from "../../../Assets/svg/icons/lock";
+import { person } from "../../../Assets/svg/icons/person";
+import { mail } from "../../../Assets/svg/icons/mail";
+interface IValues {
+  email: string;
+  password: string;
+  confirm:string
+  username:string
+}
+const commentSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "The username is too short")
+    .required("Username is required"),
+  email: Yup.string()
+    .email("The email is invalid")
+    .required("Mail is required"),
+  password: Yup.string()
+    .min(2, "The password is too short")
+    .required("Password is required")
+    .min(8, "Password must be 8 characters long")
+    .matches(/[0-9]/, "Password requires a number")
+    .matches(/[a-z]/, "Password requires a lowercase letter")
+    .matches(/[A-Z]/, "Password requires an uppercase letter"),
+  confirm: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    'Must match "password" field value'
+  ),
+});
+const Register = () => {
+  const initialValues: IValues = {
+    email: "",
+    password: "",
+    confirm:"",
+    username:"",
+  };
+  console.log(styles);
+  return (
+    <div className={styles.container}>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={commentSchema}
+        validateOnChange={true}
+        validateOnBlur={true}
+        onSubmit={(values: IValues) => {
+          console.log({ values });
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className={styles.container_form}>
+            <div className={styles.container_form_username}>
+              <Field
+                className={styles.container_form_username_field}
+                name="username"
+                type="text"
+              ></Field>
+              {errors.username && touched.username ? (
+                <div className={styles.container_form_username_error}>
+                  {errors.username}
+                </div>
+              ) : null}
+              {person(styles)}
+              <h5>Username</h5>
+            </div>
+            <div className={styles.container_form_mail}>
+              <Field
+                className={styles.container_form_mail_field}
+                name="email"
+                type="email"
+              ></Field>
+              {errors.email && touched.email ? (
+                <div className={styles.container_form_mail_error}>
+                  {errors.email}
+                </div>
+              ) : null}
+              {mail(styles)}
+              <h5>Email</h5>
+            </div>
+            <div className={styles.container_form_password}>
+              <Field
+                className={styles.container_form_password_field}
+                id="password"
+                name="password"
+                type="password"
+              />
+              {errors.password && touched.password ? (
+                <div className={styles.container_form_password_error}>
+                  {errors.password}
+                </div>
+              ) : null}
+              {lock(styles)}
+              <h5>Password</h5>
+            </div>
+            <div className={styles.container_form_password}>
+              <Field
+                className={styles.container_form_password_field}
+                id="confirm"
+                name="confirm"
+                type="password"
+              />
+              {errors.confirm && touched.confirm ? (
+                <div className={styles.container_form_password_error}>
+                  {errors.confirm}
+                </div>
+              ) : null}
+              {lock(styles)}
+              <h5>Confirm Password</h5>
+            </div>
+            <div className={styles.container_form_button}>
+              <button type="submit">
+                {/* <span>{plane()}</span>  */}
+                LOGIN
+              </button>
+            </div>
+            <div className={styles.container_form_link}>
+              <Link to={"/login"}>Have an account ?</Link>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Register;
