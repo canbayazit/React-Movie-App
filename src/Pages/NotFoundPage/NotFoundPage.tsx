9cff9c83c8056428feb876289ee79a804529d09a
Can <rovahelsing@gmail.com>
2023-03-14 06:14:50 +0300

    added not found page component

---
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./notFound.module.scss";
+
+const NotFoundPage = () => {
+  return <div className={styles.container}><h1>There is Nothing Here!</h1></div>;
+};
+
+export default NotFoundPage;
