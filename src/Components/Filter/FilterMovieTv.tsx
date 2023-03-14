c522c4645c25ebfe71ced379ec0c29f2abf85c56
Can <rovahelsing@gmail.com>
2023-03-14 06:15:57 +0300

    updated filter page

--- src/Components/Filter/FilterMovieTv.tsx
+++ src/Components/Filter/FilterMovieTv.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
 import {
   Formik,
-  FormikHelpers,
-  FormikProps,
   Form,
   Field,
-  FieldProps,
 } from "formik";
 import styles from "./filterMovieTv.module.scss";
 import {
@@ -14,22 +11,25 @@ import {
 } from "../../Service/movieServices";
 import { useParams } from "react-router";
 import MovieCard from "../Card/MovieCard/MovieCard";
+import Loading from "../Loading/Loading";
+import { toast } from "react-toastify";
 interface IValues {
   genre: number;
   imdb: number;
 }
 interface IImdb {
+  id: number;
   name: string;
   value: number;
 }
 const imdb: IImdb[] = [
-  { name: "3 and upper", value: 3 },
-  { name: "4 and upper", value: 4 },
-  { name: "5 and upper", value: 5 },
-  { name: "6 and upper", value: 6 },
-  { name: "7 and upper", value: 7 },
-  { name: "8 and upper", value: 8 },
-  { name: "9 and upper", value: 9 },
+  { id: 1, name: "3 and upper", value: 3 },
+  { id: 2, name: "4 and upper", value: 4 },
+  { id: 3, name: "5 and upper", value: 5 },
+  { id: 4, name: "6 and upper", value: 6 },
+  { id: 5, name: "7 and upper", value: 7 },
+  { id: 6, name: "8 and upper", value: 8 },
+  { id: 7, name: "9 and upper", value: 9 },
 ];
 const FilterMovieTv = () => {
   const { category } = useParams();
@@ -59,70 +59,101 @@ const FilterMovieTv = () => {
     genre: 0,
     imdb: 0,
   };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.container_header}>
-        <h1>
-          Popular {category?.charAt(0).toUpperCase() + category?.slice(1)!}s
-        </h1>
-      </div>
-      <div className={styles.container_main}>
-        <div className={styles.container_main_filter}>
-          <Formik
-            initialValues={initialValues}
-            onSubmit={(values: IValues) => {
-              console.log({ values });
-              setFilter({
-                genre: values.genre,
-                imdb: values.imdb,
-              });
-              setPage(1);
-            }}
-          >
-            <Form className={styles.container_main_filter_form}>
-              <div className={styles.container_main_filter_form_genre}>
-                <Field
-                  as="select"
-                  name="genre"
-                  className={styles.container_main_filter_form_genre_field}
-                >
-                  <option selected>Genre</option>
-                  {genres?.data?.genres.map((genre) => (
-                    <option value={genre.id}>{genre.name}</option>
-                  ))}
-                </Field>
-              </div>
-              <div className={styles.container_main_filter_form_imdb}>
-                <Field
-                  as="select"
-                  name="imdb"
-                  className={styles.container_main_filter_form_imdb_field}
-                >
-                  <option selected>IMDB Rating</option>
-                  {imdb.map((item) => (
-                    <option value={item.value}>{item.name}</option>
-                  ))}
-                </Field>
-              </div>
-              <div className={styles.container_main_filter_form_button}>
-                <button type="submit">Search</button>
-              </div>
-            </Form>
-          </Formik>
+    <>
+      <div className={styles.container}>
+        <div className={styles.container_header}>
+          <h1>
+            Popular {category?.charAt(0).toUpperCase() + category?.slice(1)!}s
+          </h1>
         </div>
-        <div className={styles.container_main_movie}>
-          {allMovieTv.data?.results.map((movie) => (
-            <div className={styles.container_main_movie_card}>
-              <MovieCard
-                movie={movie}
-                genreId={filter?.genre ?? movie.genre_ids![0]}
-                categoryType={category!}
-              />
+        <div className={styles.container_main}>
+          <div className={styles.container_main_filter}>
+            <Formik
+              initialValues={initialValues}
+              onSubmit={(values: IValues) => {
+                console.log({ values });
+                if (values.genre && values.imdb) {
+                  setFilter({
+                    genre: values.genre,
+                    imdb: values.imdb,
+                  });
+                  setPage(1);
+                } else {
+                  toast.error(
+                    `
+                    You need to choose options!`,
+                    {
+                      position: "top-right",
+                      autoClose: 5500,
+                      hideProgressBar: false,
+                      closeOnClick: true,
+                      pauseOnHover: false,
+                      draggable: true,
+                      progress: undefined,
+                      theme: "colored",
+                    }
+                  );
+                }
+              }}
+            >
+              <Form className={styles.container_main_filter_form}>
+                <div className={styles.container_main_filter_form_genre}>
+                  <Field
+                    as="select"
+                    name="genre"
+                    className={styles.container_main_filter_form_genre_field}
+                  >
+                    <option selected>Genre</option>
+                    {genres?.data?.genres.map((genre) => (
+                      <option key={genre.id} value={genre.id}>
+                        {genre.name}
+                      </option>
+                    ))}
+                  </Field>
+                </div>
+                <div className={styles.container_main_filter_form_imdb}>
+                  <Field
+                    defaultValue={"DEFAULT"}
+                    as="select"
+                    name="imdb"
+                    className={styles.container_main_filter_form_imdb_field}
+                  >
+                    <option selected value="DEFAULT">
+                      IMDB Rating
+                    </option>
+                    {imdb.map((item, i) => (
+                      <option key={item.id} value={item.value}>
+                        {item.name}
+                      </option>
+                    ))}
+                  </Field>
+                </div>
+                <div className={styles.container_main_filter_form_button}>
+                  <button type="submit">Search</button>
+                </div>
+              </Form>
+            </Formik>
+          </div>
+          {allMovieTv.isFetching && 
+            <Loading />
+          }
+            <div className={styles.container_main_movie}>
+              {allMovieTv.data?.results.map((movie) => (
+                <div className={styles.container_main_movie_card}>
+                  <MovieCard
+                    movie={movie}
+                    genreId={filter?.genre ?? movie.genre_ids![0]}
+                    categoryType={category!}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
+       
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/FilterPage/FilterPage.tsx
+++ src/Pages/FilterPage/FilterPage.tsx
@@ -6,7 +6,6 @@ import styles from './filter.module.scss';
 const FilterPage = () => {
   return (
     <div>
-        <UpcomingSlider/>
         <FilterMovieTv/>
     </div>
   )
