17b5c5a0de83010c2792304db45a4b9f7c086ae9
Can <rovahelsing@gmail.com>
2023-03-19 22:32:08 +0300

    updated person detail component according to language data and css design

--- src/Components/List/NavContents/Detail/PersonNavDetail.tsx
+++ src/Components/List/NavContents/Detail/PersonNavDetail.tsx
@@ -7,21 +7,14 @@ import { useGetDetailServiceQuery } from "../../../../Service/movieServices";
 import styles from "./personNavDetail.module.scss";
 
 const PersonNavDetail = () => {
-  const [lang, setLang] = useState<string>(i18n.language.replace("_","-"));
   const { category, id } = useParams();
   const { t } = useTranslation();
-  const { data, isLoading, isFetching } = useGetDetailServiceQuery({
+  const { data, isLoading } = useGetDetailServiceQuery({
     category: category!,
     id: id!,
-    lang:lang
+    lang: i18n.language.replace("_", "-"),
   });
-  useEffect(() => {
-    if (!isFetching) {
-      if (!data?.biography) {
-        setLang('en-EN')
-      }
-    }    
-  }, [data?.biography, isFetching])
+
   return (
     <>
       {isLoading ? (
@@ -31,31 +24,31 @@ const PersonNavDetail = () => {
           <h1>{data?.name}</h1>
           <div className={styles.container_content}>
             <div className={styles.container_content_left}>
-              <p>{data?.biography}</p>
+              <p>{data?.biography || t("notFoundBio")}</p>
             </div>
             <div className={styles.container_content_right}>
               <div className={styles.container_content_right_info}>
-                <h4>{t('department')}:</h4>
+                <h4>{t("department")}:</h4>
                 <p>{data?.known_for_department}</p>
               </div>
               <div className={styles.container_content_right_info}>
-                <h4>{t('age')}:</h4>
-                <p>{moment().diff(data?.birthday, "years")}</p>
+                <h4>{t("age")}:</h4>
+                <p>{moment().diff(data?.birthday, "years") || t("unknown")}</p>
               </div>
               <div className={styles.container_content_right_info}>
-                <h4>{t('birthday')}:</h4>
-                <p>{data?.birthday}</p>
+                <h4>{t("birthday")}:</h4>
+                <p>{data?.birthday || t("unknown")}</p>
               </div>
               {data?.deathday ? (
                 <div className={styles.container_content_right_info}>
-                  <h4>{t('dayOfDeath')}:</h4>
-                  <p>{data?.deathday}</p>
+                  <h4>{t("dayOfDeath")}:</h4>
+                  <p>{data?.deathday || t("unknown")}</p>
                 </div>
               ) : null}
 
               <div className={styles.container_content_right_info}>
-                <h4>{t('placeOfBirth')}:</h4>
-                <p>{data?.place_of_birth}</p>
+                <h4>{t("placeOfBirth")}:</h4>
+                <p>{data?.place_of_birth || t("unknown")}</p>
               </div>
             </div>
           </div>
